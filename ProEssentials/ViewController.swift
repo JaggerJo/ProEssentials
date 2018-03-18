2a7e797821a6b7810e3ee8aab69214f86a208360
JaggerJo <josuajaeger@josuas-macbook-pro-2.local>
2018-03-18 20:37:49 +1000

    Initial Commit

---
+++ ProEssentials/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProEssentials
+//
+//  Created by Josua Jaeger on 18.03.18.
+//  Copyright © 2018 Josua Jaeger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
